ef3388296969d57be86947dbfc19b9d048613c1e
Anton Khnykin <antonkhnykin@gmail.com>
2023-12-08 19:12:54 +0700

    Update

--- layers/Dense.py
+++ layers/Dense.py
@@ -1,15 +1,21 @@
-
+import numpy as np
 
 class Dense():
 
     units = 0
     activation = ""
-    input_shape = 0
+    input_shape = ()
+    W = np.zeros(0, np.float64)
+    b = np.zeros(0, np.float64)
+    outputs = np.zeros(0, np.float64)
 
-    def __init(self, units, input_shape, activation=None, ):
+    def __init(self, units, input_shape, activation=None):
         self.units = units
         self.activation = activation
         self.input_shape = input_shape
+        self.W = np.reshape(self.W, (input_shape[0], units))
+        self.b = np.reshape(self.b, units)
+        self.outputs = np.reshape(self.outputs, units)
 
         if self.units < 0:
             raise ValueError(
